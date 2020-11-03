9bbf4a487e8d4e2d47aeab8275bd9a3e3336b89e
Julen Sanchez Rosales <julenvitoria@hotmail.es>
2020-11-03 08:38:24 +0100

    ThemePlusLaunchings.sh

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Kelboy-SuperRetroBoyTheme
 SuperRetroBoy Theme &amp; Launching images
+
+Script to install [Super RetroBoy theme by KALEL1981](https://github.com/KALEL1981/es-theme-Super-Retroboy) and launchings generateds for each systems 
+
+How to install 
+Connect via ssh or insert this command via terminak on your freeplay 
+
+wget -O- https://raw.githubusercontent.com/julenvitoria/Kelboy-SuperRetroBoyTheme/master/ThemePlusLaunchings.sh | bash 
+
+
+NOTE: IT'S NECCESARY THAT YOUR GBA HAS INTERNET CONNECTION. THIS SCRIPT WORKS WITH ABSOLUTE PATHS. 
---
+++ ThemePlusLaunchings.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+cd /home/pi
+if [ -d "/home/pi/.emulationstation/themes/Super Retroboy/" ]; then
+        echo "Super Retroboy theme was downloaded yet"
+        echo "Deleting and redownloading..."
+        sleep 2
+        sudo rm -r "/home/pi/.emulationstation/themes/Super Retroboy/"
+        sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
+        cd "/opt/retropie/configs/all/emulationstation/themes/Super Retroboy/"
+        sudo sed -i -e 's/<fontSize>0.042/<fontSize>0.06/g' SuperRetroboy.xml
+        sudo sed -i -e 's/<fontSize>0.025/<fontSize>0.035/g' SuperRetroboy.xml
+        sudo sed -i -e 's/<fontSize>0.04/<fontSize>0.055/g' SuperRetroboy.xml
+else
+        echo "Downloading Super Retroboy theme..."
+        sleep 2
+        sudo mkdir -p "/home/pi/.emulationstation/themes/Super Retroboy/" && sudo git clone https://github.com/KALEL1981/Super_Retroboy_Theme.git "/home/pi/.emulationstation/themes/Super Retroboy/" --branch master --depth=1
+        cd "/opt/retropie/configs/all/emulationstation/themes/Super Retroboy/"
+        sudo sed -i -e 's/<fontSize>0.042/<fontSize>0.06/g' SuperRetroboy.xml
+        sudo sed -i -e 's/<fontSize>0.025/<fontSize>0.035/g' SuperRetroboy.xml
+        sudo sed -i -e 's/<fontSize>0.04/<fontSize>0.055/g' SuperRetroboy.xml
+fi
+cd /home/pi
+if [ -d /home/pi/tmp ]; then
+        sudo rm -R /home/pi/tmp
+        mkdir /home/pi/tmp
+else
+        mkdir /home/pi/tmp
+fi
+git clone https://github.com/julenvitoria/FreeplayGBA-SuperRetroBoyTheme "/home/pi/tmp" --branch master --depth=1
+echo "COPYING LAUNCHING IMAGES..."
+sleep 3
+cp -R /home/pi/tmp/configs/* /opt/retropie/configs
+sudo rm -r tmp
+/home/pi/scripts/multi_switch.sh --ES-RESTART
